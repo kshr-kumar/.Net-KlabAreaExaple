022fcf332124a90c050c9753bf0b25be4ab88781
UI <UI@UI>
2024-08-07 17:47:06 +0530

    Add project files.

---
+++ AreaExaple/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AreaExaple.Areas.Admin.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
